6b9d839f418c16be129e8a94bf4bec7f92cc6546
shyam munda <mastershyam2013@gmail.com>
2020-06-25 05:55:07 +0530

    Updates

---
+++ precache-manifest.cc9247d40c7d35729728a70fbb20a178.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d96fc3ed7028eade308c9fb450a4b13a",
+    "url": "/woven/index.html"
+  },
+  {
+    "revision": "e90d670799243485ac96",
+    "url": "/woven/static/css/2.aae3c489.chunk.css"
+  },
+  {
+    "revision": "7fa8958cbee7c96e475a",
+    "url": "/woven/static/css/main.8c70653a.chunk.css"
+  },
+  {
+    "revision": "e90d670799243485ac96",
+    "url": "/woven/static/js/2.58db5f43.chunk.js"
+  },
+  {
+    "revision": "f7ca4bf62dab730414a11c7ee9da82d4",
+    "url": "/woven/static/js/2.58db5f43.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7fa8958cbee7c96e475a",
+    "url": "/woven/static/js/main.d415fbe0.chunk.js"
+  },
+  {
+    "revision": "726cd910f58c61f42a97",
+    "url": "/woven/static/js/runtime-main.307311e6.js"
+  },
+  {
+    "revision": "34e2201fa654bae29997b9bbe7fd92e2",
+    "url": "/woven/static/media/01_Cre_tall.34e2201f.jpg"
+  },
+  {
+    "revision": "7b4c04219b503e6269e4f415e74d1e45",
+    "url": "/woven/static/media/AceJigtall01.7b4c0421.jpg"
+  },
+  {
+    "revision": "91b5567dfe6efbf8a66fed119eda867d",
+    "url": "/woven/static/media/AceJigtall02.91b5567d.jpg"
+  },
+  {
+    "revision": "914d5652c04c86e7116525d70ad44392",
+    "url": "/woven/static/media/Antelopehome.914d5652.jpg"
+  },
+  {
+    "revision": "0dcb100485363d9620ca20797875a704",
+    "url": "/woven/static/media/CobraRockhome01.0dcb1004.jpg"
+  },
+  {
+    "revision": "632bcf094e54054cfb8abfd5d0a792fd",
+    "url": "/woven/static/media/CodyCobbhome.632bcf09.jpg"
+  },
+  {
+    "revision": "e21ecf1e038a85398c54ab8e40085897",
+    "url": "/woven/static/media/CodyCobbhome01.e21ecf1e.jpg"
+  },
+  {
+    "revision": "71f4b2ea9ed7671ba3678cfdb2fc0665",
+    "url": "/woven/static/media/Cover.71f4b2ea.jpg"
+  },
+  {
+    "revision": "2fbe2e6fcea95c89f257e8a69fcabc39",
+    "url": "/woven/static/media/Cover1.2fbe2e6f.jpg"
+  },
+  {
+    "revision": "d5617ab117543fb233799d6a2de3cfca",
+    "url": "/woven/static/media/Cre_home.d5617ab1.jpg"
+  },
+  {
+    "revision": "965843f68f027edc13f27e108d01d232",
+    "url": "/woven/static/media/Cre_tall07.965843f6.jpg"
+  },
+  {
+    "revision": "9fa82d649ec29a55238ca09e537e5f85",
+    "url": "/woven/static/media/Cretall02.9fa82d64.jpg"
+  },
+  {
+    "revision": "fdb56d4aaf0b693b04ac68ff67b68332",
+    "url": "/woven/static/media/Cretall04.fdb56d4a.jpg"
+  },
+  {
+    "revision": "e6c8634a3e44e75dfbff825bde8cac46",
+    "url": "/woven/static/media/Cretall05.e6c8634a.jpg"
+  },
+  {
+    "revision": "fcff83dd7cf7403d2472252c74db8789",
+    "url": "/woven/static/media/Cretall06.fcff83dd.jpg"
+  },
+  {
+    "revision": "f29ed2a8bbccb9578ebe838ee6ac7b93",
+    "url": "/woven/static/media/Cretall08.f29ed2a8.jpg"
+  },
+  {
+    "revision": "9604af0dc1046087e6e9595fcfebd214",
+    "url": "/woven/static/media/Cretall09.9604af0d.jpg"
+  },
+  {
+    "revision": "aa88b4c9485f12b85ef4dc7b2391fabc",
+    "url": "/woven/static/media/Cretall11.aa88b4c9.jpg"
+  },
+  {
+    "revision": "5514a7f21b1aff6d87d460ed3fbd1afa",
+    "url": "/woven/static/media/Cretall12.5514a7f2.jpg"
+  },
+  {
+    "revision": "9a0d8951a67a796abd67357301dee070",
+    "url": "/woven/static/media/Crewide01.9a0d8951.jpg"
+  },
+  {
+    "revision": "012d11390b958c3049ec5541943a058d",
+    "url": "/woven/static/media/Crewide02.012d1139.jpg"
+  },
+  {
+    "revision": "3633a99b715c87e5f39d471115eb25af",
+    "url": "/woven/static/media/IssueCove.3633a99b.jpg"
+  },
+  {
+    "revision": "7455b49e7e37c1672852525b1cf5f7f5",
+    "url": "/woven/static/media/JDRhome01.7455b49e.jpg"
+  },
+  {
+    "revision": "01772c8c2415bc9d7d12394b3101d453",
+    "url": "/woven/static/media/Joekennethhome.01772c8c.jpg"
+  },
+  {
+    "revision": "dd702e6213d4270e5ec72f5e5377d863",
+    "url": "/woven/static/media/JulianneAhnhome.dd702e62.jpg"
+  },
+  {
+    "revision": "dff49c4031c89248b4660d2638159f2f",
+    "url": "/woven/static/media/JustinChung_home.dff49c40.jpg"
+  },
+  {
+    "revision": "0f7f645c7624eac3ce0c866e9f2dc65f",
+    "url": "/woven/static/media/KyleSteedcoverhome01.0f7f645c.jpg"
+  },
+  {
+    "revision": "e6e264f58eac6c672e8bf4615ba7e2b2",
+    "url": "/woven/static/media/Layer.e6e264f5.jpg"
+  },
+  {
+    "revision": "b7f9dc951eac4c5cc933695f5b057ea2",
+    "url": "/woven/static/media/Layereight.b7f9dc95.jpg"
+  },
+  {
+    "revision": "63cbb5d514244815311c840bc7abb418",
+    "url": "/woven/static/media/Layerone.63cbb5d5.jpg"
+  },
+  {
+    "revision": "2c00951ba31451ad5a2c8391bafd4689",
+    "url": "/woven/static/media/Layerseven.2c00951b.jpg"
+  },
+  {
+    "revision": "0a440aa16cf4627fb1b360cc97c1d3eb",
+    "url": "/woven/static/media/MuireCover.0a440aa1.jpg"
+  },
+  {
+    "revision": "9a524c56b58c63aeeb2328789f2f750b",
+    "url": "/woven/static/media/ShantellMartin.9a524c56.jpg"
+  },
+  {
+    "revision": "9f500fe69f0d5aebebbd0d88537e8e28",
+    "url": "/woven/static/media/ShantellMartinall.9f500fe6.jpg"
+  },
+  {
+    "revision": "360261b2afbc91586d10e0ec61aa1c23",
+    "url": "/woven/static/media/ShantellMartintall.360261b2.jpg"
+  },
+  {
+    "revision": "d109293d910fb7373ca0e08e91a4e3d3",
+    "url": "/woven/static/media/ShantellMartintallfive.d109293d.jpg"
+  },
+  {
+    "revision": "676ca8571a50ff8839388250ab7d225d",
+    "url": "/woven/static/media/ShantellMartintallfour.676ca857.jpg"
+  },
+  {
+    "revision": "477bc079a6d113662ed54cfd036e1ea0",
+    "url": "/woven/static/media/ShantellMartintallseven.477bc079.jpg"
+  },
+  {
+    "revision": "db74e633320443433f6605a6ab880875",
+    "url": "/woven/static/media/ShantellMartintallsix.db74e633.jpg"
+  },
+  {
+    "revision": "3e2fda38ce04411f1e3e3a4abd426db1",
+    "url": "/woven/static/media/WV_home.3e2fda38.jpg"
+  },
+  {
+    "revision": "bc8dbedce3bc15ea3d391517be97325a",
+    "url": "/woven/static/media/campfirecover.bc8dbedc.jpg"
+  },
+  {
+    "revision": "a938dc89a127c1f6e88948848e7ad85f",
+    "url": "/woven/static/media/canvascotton.a938dc89.jpg"
+  },
+  {
+    "revision": "9d7f1dd1b1cec26289639bdb111fc58f",
+    "url": "/woven/static/media/econ_earth.9d7f1dd1.jpg"
+  },
+  {
+    "revision": "18d6aa6a2f987eda459a9909255cc072",
+    "url": "/woven/static/media/firstcover.18d6aa6a.jpg"
+  },
+  {
+    "revision": "edd6c7617ff2ee146cef1c11fb9044ac",
+    "url": "/woven/static/media/groupwork.edd6c761.jpg"
+  },
+  {
+    "revision": "e0079c4f83ff0d6fab2804d901999dd7",
+    "url": "/woven/static/media/icon-twitter.e0079c4f.png"
+  },
+  {
+    "revision": "5b0106868fe55fde320078dc68166cbd",
+    "url": "/woven/static/media/issue2cover.5b010686.jpg"
+  },
+  {
+    "revision": "2d404004f699cd5f32ab6789176eaa8e",
+    "url": "/woven/static/media/issue3shop.2d404004.jpg"
+  },
+  {
+    "revision": "0d1ace7cefd4f3489e8fb07338bfa899",
+    "url": "/woven/static/media/logo.0d1ace7c.svg"
+  },
+  {
+    "revision": "28ad0647b3bba3e5975b07791d83b833",
+    "url": "/woven/static/media/peacefulstills.28ad0647.jpg"
+  },
+  {
+    "revision": "af30ddf43df2c3f90f9793b86a3a844d",
+    "url": "/woven/static/media/postcardscover.af30ddf4.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/woven/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/woven/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/woven/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/woven/static/media/slick.f97e3bbf.svg"
+  }
+]);
